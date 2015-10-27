88b2e52f2d2744774730015c089987452c576aaf
Azans3cr3t <avhiru84jazz@gmail.com>
2015-10-27 17:20:49 +0530

    added Group class with getters and setters

---
+++ app/src/main/java/com/example/android/login/Group.java
@@ -0,0 +1,26 @@
+package com.example.android.login;
+
+/**
+ * Created by avhirup on 10/27/2015.
+ */
+public class Group {
+
+    public Group() {
+    }
+    //Declarations
+    private int group_id;
+    private String group_name;
+    //Method Implementation
+
+    public int getGroupID() {
+        return group_id;
+    }
+
+    public String getGroupName() {
+        return group_name;
+    }
+
+    public void setGroupName(String group_name){
+        this.group_name=group_name;
+    }
+}
