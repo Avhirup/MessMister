0f8aafad0292d712fb848ea1d4e053841c95a256
Azans3cr3t <avhiru84jazz@gmail.com>
2015-10-27 17:12:37 +0530

    Added Expense & Owner class-methos implementataion remaining

---
+++ app/src/main/java/com/example/android/login/Expense.java
@@ -0,0 +1,34 @@
+package com.example.android.login;
+
+/**
+ * Created by avhirup on 10/27/2015.
+ */
+public class Expense{
+
+    //Declarations
+    private static int expense_id;
+    private String expense_name;
+    private int amount;
+
+    //Methods
+    public void setExpName(String name) {
+        expense_name=name;
+    }
+
+    public String getExpName() {
+        return expense_name;
+    }
+
+    public int getExpID() {
+        return expense_id;
+    }
+
+    public void setAmount(int amount) {
+        this.amount=amount;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+}
