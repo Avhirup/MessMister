d0e98e4bf4ecf6dd3cbf947a6a891ab0e6f18191
codeNMaggi <medhadnaik@gmail.com>
2015-10-27 18:58:59 +0530

    new

--- app/src/main/java/com/example/android/login/MessMember.java
+++ app/src/main/java/com/example/android/login/MessMember.java
@@ -66,7 +66,7 @@ public class MessMember {
         }
 
         /**
-         * @return
+         * @return.
          */
         public String getStartDate() {
             // TODO implement here
