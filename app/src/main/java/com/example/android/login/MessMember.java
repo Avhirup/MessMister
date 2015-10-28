7728223069b10a59cb3106fe9d8dcc057364b597
manjusha23 <manjushapednekar23@gmail.com>
2015-10-28 09:36:11 +0530

    Messmember methods changed, Messmeberhepler added

--- app/src/main/java/com/example/android/login/MessMember.java
+++ app/src/main/java/com/example/android/login/MessMember.java
@@ -8,11 +8,6 @@ import java.util.Vector;
 public class MessMember {
 
 
-
-
-        /**
-         * Default constructor
-         */
         public MessMember() {
         }
 
@@ -29,119 +24,93 @@ public class MessMember {
         private float due_amount;
         private String phone;
 
-
-
-        /**
-         * @param name
-         * @return
-         */
-        public void setName(String name) {
-            // TODO implement here
-
+        public String getStart_date() {
+                return start_date;
         }
 
-        /**
-         * @return
-         */
+        public void setStart_date(String start_date) {
+                this.start_date = start_date;
+        }
+
         public String getName() {
-            // TODO implement here
-            return "";
+                return name;
         }
 
-        /**
-         * @return
-         */
-        public String getStartDate() {
-            // TODO implement here
-            return "";
+        public void setName(String name) {
+                this.name = name;
         }
 
-        /**
-         * @return
-         */
-        public void setStartDate(String s) {
-            // TODO implement here
-
+        public int getMember_id() {
+                return member_id;
         }
 
-        /**
-         * @return
-         */
-        public Boolean getIsActive() {
-            // TODO implement here
-            return null;
+        public void setMember_id(int member_id) {
+                this.member_id = member_id;
         }
 
-        /**
-         * @return
-         */
-        public void setIsActive(Boolean b) {
-            // TODO implement here
-
+        public String getStartof_month() {
+                return startof_month;
         }
 
-
-
-        /**
-         * @return
-         */
-        public void setGroup(Vector<Integer> groups) {
-            // TODO implement
+        public void setStartof_month(String startof_month) {
+                this.startof_month = startof_month;
         }
 
-        public Vector<Integer> groupsgetGroup() {
-        // TODO implement h
-            return  null;
+        public Boolean getIs_active() {
+                return is_active;
         }
 
-        /**
-         * @return
-         */
-        public int getRate() {
-            // TODO implement here
-            return 0;
+        public void setIs_active(Boolean is_active) {
+                this.is_active = is_active;
         }
 
-        public void setRateId(int id)
-        {
-
+        public int getRate_id() {
+                return rate_id;
         }
 
-        /**
-         * @return
-         */
-        public float getDueAmount() {
-            // TODO implement here
-            return 0;
-
+        public void setRate_id(int rate_id) {
+                this.rate_id = rate_id;
         }
 
-        public void setDueAmount(float amt)
-        {
-
+        public Boolean getHas_paid() {
+                return has_paid;
         }
 
-        /**
-         * @return
-         */
-        public Boolean checkIfLate() {
-            // TODO implement here
-            return null;
+        public void setHas_paid(Boolean has_paid) {
+                this.has_paid = has_paid;
         }
 
-        public int getMemberId()
-        {
-            return member_id;
+        public Boolean getIs_late() {
+                return is_late;
         }
 
-        public String getPhone()
-        {
-            return phone;
+        public void setIs_late(Boolean is_late) {
+                this.is_late = is_late;
         }
 
-        public void setPhone(String phone)
-        {
-
+        public float getDue_amount() {
+                return due_amount;
         }
-    }
+
+        public void setDue_amount(float due_amount) {
+                this.due_amount = due_amount;
+        }
+
+        public String getPhone() {
+                return phone;
+        }
+
+        public void setPhone(String phone) {
+                this.phone = phone;
+        }
+
+        public Vector<Integer> getGroup_id() {
+                return group_id;
+        }
+
+        public void setGroup_id(Vector<Integer> group_id) {
+                this.group_id = group_id;
+        }
+
+}
 
