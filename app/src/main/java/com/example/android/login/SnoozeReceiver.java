b1a91468bb452c903b4ae30232f19f27a0b6b44a
Azans3cr3t <avhiru84jazz@gmail.com>
2015-11-12 12:37:11 +0530

    Notification Services

---
+++ app/src/main/java/com/example/android/login/SnoozeReceiver.java
@@ -0,0 +1,15 @@
+package com.example.android.login;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class SnoozeReceiver extends BroadcastReceiver {
+    public SnoozeReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
