75fbaaa746bdf03da90d0c475ddd5d3d2f251bd0
codeNMaggi <medhadnaik@gmail.com>
2015-11-21 14:19:12 +0530

    final2

--- app/src/main/java/com/example/android/login/MyDialog.java
+++ app/src/main/java/com/example/android/login/MyDialog.java
@@ -17,30 +17,54 @@ public class MyDialog
     Context context;
     AlertDialog.Builder alert;
     LayoutInflater factory;
+    View textEntryView;
+    MemberDatabase memberDatabase;
+    RateDataBase rateDataBase;
+    AutoCompleteTextView amount_paid;
+    AutoCompleteTextView member_fee_name;
+    CharSequence text = "already paid";
+    Toast toast;
+    int duration = Toast.LENGTH_SHORT;
     int type;
     String titles[] = {"  Add New Member Fee","  Add Quick Income", "  Add Quick Expense"};
-    public MyDialog(Context context, int type)
-    {
+    public MyDialog(Context context, int type) {
         this.context = context;
         this.type = type;
         factory = LayoutInflater.from(context);
         alert = new AlertDialog.Builder(context);
-        final View textEntryView = factory.inflate(R.layout.edittext3,null);
+        textEntryView = factory.inflate(R.layout.edittext3, null);
         alert.setView(textEntryView);
         setTitle();
-        final MemberDatabase memberDatabase = new MemberDatabase(context);
-        final RateDataBase rateDataBase = new RateDataBase(context);
-
-        final AutoCompleteTextView member_fee_name = (AutoCompleteTextView)textEntryView.findViewById(R.id.member_fee_name);
-        final AutoCompleteTextView amount_paid = (AutoCompleteTextView)textEntryView.findViewById(R.id.amount_paid);
-
-        CharSequence text = "already paid";
-        int duration = Toast.LENGTH_SHORT;
-
-        final Toast toast = Toast.makeText(context,text,duration);
-
-
+        memberDatabase = new MemberDatabase(context);
+        rateDataBase = new RateDataBase(context);
+        member_fee_name = (AutoCompleteTextView) textEntryView.findViewById(R.id.member_fee_name);
+        amount_paid = (AutoCompleteTextView) textEntryView.findViewById(R.id.amount_paid);
+        toast = Toast.makeText(context, text, duration);
         member_fee_name.setThreshold(0);
+
+        if (type == 0)
+            getMemberValues();
+        else if(type == 1)
+            getIncomeValues();
+        else if(type == 2)
+            getExpenseValues();
+
+
+    }
+
+
+
+    public void show(){
+
+        alert.show();
+    }
+
+    public void setTitle() {
+        alert.setTitle(titles[type]);
+    }
+
+    public void getMemberValues()
+    {
         ArrayAdapter<String> adapter = new ArrayAdapter<>(context,
                 android.R.layout.simple_dropdown_item_1line, new MemberDatabase(context).getAllMembers());
 
@@ -96,14 +120,122 @@ public class MyDialog
 
     }
 
-    public void show(){
 
-        alert.show();
-    }
-
-    public void setTitle()
+    public void getIncomeValues()
     {
-        alert.setTitle(titles[type]);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(context,
+                android.R.layout.simple_dropdown_item_1line, new MemberDatabase(context).getAllMembers());
+
+        member_fee_name.setAdapter(adapter);
+
+
+        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        try {
+
+
+                            MessMember m = new MessMember();
+
+
+                            int mid, rid, amt, dueamt, mdueamt;
+                            String name = member_fee_name.getText().toString();
+                            String amount = amount_paid.getText().toString();
+
+                            int paidamt = Integer.parseInt(amount);
+                            m.setName(name);
+
+                            mid = memberDatabase.getMemberId(m);
+                            rid = memberDatabase.getrate_id(mid);
+
+
+                            amt = rateDataBase.getamt(rid);
+                            mdueamt = memberDatabase.getdue_amt(mid);
+                            if (mdueamt != 0) {
+                                dueamt = mdueamt - paidamt;
+                                memberDatabase.setDueamt(mid, dueamt);
+                            } else {
+                                toast.show();
+                            }
+
+                        } catch (Exception e) {
+                        }
+
+                    }
+                }
+
+        );
+
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener()
+
+                {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        dialog.cancel();
+                    }
+                }
+
+        );
+
+
     }
 
-}
+    public void getExpenseValues()
+    {
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(context,
+                android.R.layout.simple_dropdown_item_1line, new MemberDatabase(context).getAllMembers());
+
+        member_fee_name.setAdapter(adapter);
+
+
+        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        try {
+
+
+                            MessMember m = new MessMember();
+
+
+                            int mid, rid, amt, dueamt, mdueamt;
+                            String name = member_fee_name.getText().toString();
+                            String amount = amount_paid.getText().toString();
+
+                            int paidamt = Integer.parseInt(amount);
+                            m.setName(name);
+
+                            mid = memberDatabase.getMemberId(m);
+                            rid = memberDatabase.getrate_id(mid);
+
+
+                            amt = rateDataBase.getamt(rid);
+                            mdueamt = memberDatabase.getdue_amt(mid);
+                            if (mdueamt != 0) {
+                                dueamt = mdueamt - paidamt;
+                                memberDatabase.setDueamt(mid, dueamt);
+                            } else {
+                                toast.show();
+                            }
+
+                        } catch (Exception e) {
+                        }
+
+                    }
+                }
+
+        );
+
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener()
+
+                {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        dialog.cancel();
+                    }
+                }
+
+        );
+
+    }
+    }
+
+
+
+
+
