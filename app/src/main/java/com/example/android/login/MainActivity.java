e4e1580b6dea690c6cf05bd11af31f2465c13ac5
codeNMaggi <medhadnaik@gmail.com>
2015-11-24 12:25:13 +0530

    login toast

--- app/src/main/java/com/example/android/login/MainActivity.java
+++ app/src/main/java/com/example/android/login/MainActivity.java
@@ -14,6 +14,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -97,6 +98,10 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
             cursor.close();
             finish();
+        }
+        else
+        {
+            Toast.makeText(this, "Incorrect Username Or Password!", Toast.LENGTH_SHORT).show();
         }
 
 
